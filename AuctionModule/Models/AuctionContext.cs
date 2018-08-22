be3d9ccbce454e8f94070abe547798b7ff1f827c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-22 16:09:37 +0500

    Added New Class for the Making Bridge Classs

---
+++ AuctionModule/Models/AuctionContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace AuctionModule.Models
+{
+    public class AuctionContext : DbContext
+    {
+        public AuctionContext() : base("name=constr")
+        {
+
+        }
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
