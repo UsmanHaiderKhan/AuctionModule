1d3234dc730eecbfaf71e8d1b73c864b461ee2e6
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-22 16:00:45 +0500

    Add project files.

---
+++ AuctionModule/Controllers/AuctionsController.cs
@@ -0,0 +1,42 @@
+﻿using AuctionModule.Models;
+using System.Web.Mvc;
+
+namespace AuctionModule.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        // GET: Auctions
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Auction()
+        {
+            var categoryList = new SelectList(new[] { "Games", "Mobiles", "Movies", "Drinks", "Fabrics" });
+            ViewBag.cate = categoryList;
+            return View();
+        }
+        public ActionResult Auction([Bind(Exclude = "CurrentPrice")]Auction auction)
+        {
+            if (string.IsNullOrWhiteSpace(auction.Title))
+            {
+                ModelState.AddModelError("Title", "Title Must  be Required");
+            }
+            else if (auction.Title.Length < 5 || auction.Title.Length > 200)
+            {
+                ModelState.AddModelError("Title", "Your Title Must be Greater Then 5 and Less then 200 char");
+            }
+
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ AuctionModule/Models/Auction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuctionModule.Models
+{
+    public class Auction
+    {
+        public long Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+
+        public DateTime StartTime { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime EndTime { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal? CurrentPrice { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal StartPrice { get; set; }
+
+        public string Category { get; set; }
+
+
+    }
+}
